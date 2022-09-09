0c1a01fc07b4e195592c357a9e11a4e3532c8690
yonatanfitaw <yfitaw@gmail.com>
2022-09-08 17:25:31 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#include <stdio.h.>
+
+/**                                                                           
+
+*main-print the string in the function                                        
+
+*                                                                             
+
+*description: using the main function                                         
+
+*this program prints "programming is like building a multilingual puzzle      
+
+*return: 0                                                                    
+
+*/                                                                            
+
+int main(void)                                                                
+
+{                                                                             
+
+    puts("\"programming is like building a multilingual puzzle");             
+
+     return(0);                                                               
+
+}                                                                             
+
