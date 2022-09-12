d7027b8bbcba293b886f4b799f9e0b1dae6d3d5e
yonatanfitaw <yfitaw@gmail.com>
2022-09-12 03:39:11 -0700

    asseble

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main-print the string in the function
  * description: using the main function
- * this program prints "programming is like building a multilingual puzzle
+ * this program prints "programming is like multilingual puzzle
  * Return: Always 0 (Success)
  */
 int main(void)
