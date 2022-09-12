17a75f97082e175d549635a34b3482d161b14874
yonatanfitaw <yfitaw@gmail.com>
2022-09-12 04:15:19 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
-*main-print the string in the put function
-*
-*Description: using the function
-*this program prints "programming is like building a multilingual puzzle
-*Return: 0
+* main - Entry point
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-
-  printf("with proper grammer,but the outcome is a piece of art,\n");
-   return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
