42a1a14832168ef27334dd79d185bbc19ee475a2
yonatanfitaw <111417234+yonatanfitaw@users.noreply.github.com>
2022-09-15 10:57:54 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
 #include <stdio.h>
@@ -11,4 +12,4 @@ void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
 
-#endif /* MAIN_H */
+#endif
