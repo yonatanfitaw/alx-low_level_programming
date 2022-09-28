d2f71da0232fa5706f07a18f9ed544d612d26375
yonatanfitaw <111417234+yonatanfitaw@users.noreply.github.com>
2022-09-28 12:21:43 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,29 +1,22 @@
-#ifndef FILE main.h
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#define FILE main.h
+/**
+ * File: holberton.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-recursion directory.
+ */
 
-
-
+int _putchar(char c);
 void _puts_recursion(char *s);
-
 void _print_rev_recursion(char *s);
-
-void _putchar(char c);
-
 int _strlen_recursion(char *s);
-
 int factorial(int n);
-
 int _pow_recursion(int x, int y);
-
 int _sqrt_recursion(int n);
-
-int _sqrt(int n, int i);
-
+int is_prime_number(int n);
+int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-int is_prime_number(int n);
-
-int is_palindrome(char *s);
-
+#endif
 
