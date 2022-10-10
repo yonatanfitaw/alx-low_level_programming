2ccc3406f04a624c59e5c5b24d588561b21c7251
yonatanfitaw <yfitaw@gmail.com>
2022-10-10 04:11:00 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - ...
+ * @d: ...
+ * Return: ...
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
