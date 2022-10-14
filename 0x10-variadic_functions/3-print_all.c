45ff75ff5f423bbd4cd2049c3237b577b570bb41
yonatanfitaw <111417234+yonatanfitaw@users.noreply.github.com>
2022-10-14 13:51:33 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <stdarg.h>
-
 #include "variadic_functions.h"
 /**
  * print_all - print char, integer, float and string
@@ -10,9 +8,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-
 	unsigned int j = 0, start = 0;
-
 	char *p;
 
 	va_start(list, format);
