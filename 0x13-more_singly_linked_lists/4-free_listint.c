a3f0bbda3a45cf60b8b63249b9d63d71e91806b9
yonatanfitaw <111417234+yonatanfitaw@users.noreply.github.com>
2022-10-25 16:28:52 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,20 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * free_listint - Frees a list_t list
- * @head: Pointer to the head/first node of list_t
- */
+ * free_listint - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
+
 void free_listint(listint_t *head)
 {
-	listint_t *placeholder;
-	while (head)
+	listint_t *tp, *tp2;
+
+	tp = head;
+	while (tp != NULL)
 	{
-		placeholder = head->next;
-		free(head);
-		head = placeholder;
+		tp2 = tp->next;
+		free(tp);
+		tp = tp2;
 	}
 }
