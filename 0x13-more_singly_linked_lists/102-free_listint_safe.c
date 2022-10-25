eda7e0cbe782eca4c987af0a2cb5c91c12395d69
yonatanfitaw <111417234+yonatanfitaw@users.noreply.github.com>
2022-10-25 16:05:33 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,81 +1,41 @@
 #include "lists.h"
 
-
-
 /**
- *
- *  * free_listint_safe - thsi function free a list in safe mode
- *
- *   * @h: the head of list
- *
- *    * Description: this function free a string in a safe mode
- *
- *     * section header: the header of this function is lists.h)*
- *
- *      * Return: the size of the list
- *
- *       */
-
-
+ * free_listint_safe - thsi function free a list in safe mode
+ * @h: the head of list
+ * Description: this function free a string in a safe mode
+ * section header: the header of this function is lists.h)*
+ * Return: the size of the list
+ */
 
 size_t free_listint_safe(listint_t **h)
-
 {
+	listint_t *tmp, *actual;
+	size_t i;
+	int rest;
 
-		listint_t *tmp, *actual;
+	i = 0, actual = *h;
 
-			size_t i;
+	while (actual)
+	{
+		rest = actual - actual->next;
+		if (rest > 0)
+		{
+			tmp = actual->next;
+			free(actual);
+			actual = tmp;
+			i++;
+		} else
+		{
+			free(actual);
+			*h = NULL;
+			i++;
+			break;
+		}
 
-				int rest;
+	}
 
+	*h = NULL;
 
-
-					i = 0, actual = *h;
-
-
-
-						while (actual)
-
-								{
-
-											rest = actual - actual->next;
-
-													if (rest > 0)
-
-																{
-
-																				tmp = actual->next;
-
-																							free(actual);
-
-																										actual = tmp;
-
-																													i++;
-
-																															} else
-
-																																		{
-
-																																						free(actual);
-
-																																									*h = NULL;
-
-																																												i++;
-
-																																															break;
-
-																																																	}
-
-
-
-																																}
-
-
-
-							*h = NULL;
-
-
-
-								return (i);
-
+	return (i);
 }
